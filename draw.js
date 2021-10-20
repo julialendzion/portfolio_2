ea17df12d94449e1342a2a44b0073d9cb3e8d802
Julia Lendzion <juli47g3@stud.kea.dk>
2021-10-20 15:21:29 +0200

    animation works on scroll!!

--- draw.js
+++ draw.js
@@ -11,6 +11,22 @@ function loadSVG() {
       // TODO: put the SVG into the DOM
       document.querySelector("#svg_path").innerHTML = svgData;
       // TODO: Start the animation
-      runAnimation();
+      // runAnimation();
     });
 }
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const square = entry.target.querySelector("#svg_path");
+
+    if (entry.isIntersecting) {
+      square.classList.add("draw");
+      return; // if we added the class, exit the function
+    }
+
+    // We're not intersecting, so remove the class!
+    square.classList.remove("draw");
+  });
+});
+
+observer.observe(document.querySelector("#imgdiv"));
